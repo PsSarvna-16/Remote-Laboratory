7d762a21fb0fb419f15bb80337756ca5eb8b5d78
PsSarvna-16 <pssarvna.kumr@gmail.com>
2021-04-12 17:23:34 +0530

    Added Methods for server
    acceptClients
    closeCon

--- Server_Script.py
+++ Server_Script.py
@@ -1,34 +1,48 @@
 import socket
 from tkinter import *
+import time
 
 root = Tk()
 root.title("Remote-Lab-Server")
 
-
 class Socket():
-	name = ""
-	ip = ""
-	port = ""
-	soc = ""
 
-	def __init__(self,port):
+	def __init__(self,port,Interface):
+		self.port = port
 		self.soc = socket.socket()
+		Interface.send("Socket Created..")
 		self.name = socket.gethostname()
-		self.ip = socket.gethostbyname(name)
-		self.soc.bind(('',port))
-		self.soc.listen(1)
+		self.ip = socket.gethostbyname(self.name)
+		self.soc.bind(('',self.port))
+		Interface.send(f"\nName : {self.name} \nIP   : {self.ip}\nPort : {self.port}")
+		
 
+	def acceptClients(self,count):
+		self.soc.listen(count)
+		Interface.send("\nWaiting For Clients..")
+
+	def closeCon(self):
+		self.soc.close()
+		Interface.send("\nSocket Closed !!")
 
 class TkFrame:
 	
 	def __init__(self,root):
 		Main = Frame(root)
 
-		self.text = Text(Main ,width = 40,height = 30,background= "#EBEBEB")
+		self.text = Text(Main ,width = 40,height = 25,state = DISABLED,background= "#EBEBEB",xscrollcommand = True)
 		self.text.grid()
 
 		Main.grid()
 
+	def send(self,msg):
+		self.text.config(state = NORMAL)
+		self.text.insert(END ,msg + "\n")
+		self.text.config(state = DISABLED)
+
 
 Interface = TkFrame(root)
+ser = Socket(6666,Interface)
+ser.acceptClients(3)
+ser.closeCon()
 root.mainloop()
